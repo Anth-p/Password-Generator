e45865c9a36cc9fa42505ab9e186620e3407eb47
Anthony <anthonypalermo@Anthonys-MacBook-Air.local>
2022-07-20 20:42:26 -0400

    generate password using selected criteria

--- script.js
+++ script.js
@@ -21,13 +21,30 @@ function writePassword() {
     var passwordLength = prompt("Choose a password length of at least 8 characters and no more than 128 characters.");
   }
 
-  // If clicked cancel for password criteria 
+  // If cancel is clicked for password criteria 
   else if (lowercaseLett === false && uppercaseLett === false && num === false && specialChar === false) {
     alert("You must chose at least one password criteria.");
     var lowercaseLett = confirm("Do you want your password to contain lowercase letters?");
     var uppercaseLett = confirm("Do you want your password to contain uppercase letters?");
     var num = confirm("Do you want your password to contain numbers?");
     var specialChar = confirm("Do you want your password to contain special characters?");
+  }
+
+  // Using criteria selected to generate password
+  if (lowercaseLett) {
+      optionsVariable += lowercaseAlphabet;
+  }
+
+  if (uppercaseLett) {
+      optionsVariable += uppercaseAlphabet;
+  }
+
+  if (num) {
+      optionsVariable += numbers;
+  }
+
+  if (specialChar) {
+      optionsVariable += specialCharacters;
   }
 
   
